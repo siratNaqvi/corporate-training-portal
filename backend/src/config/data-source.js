dd733aed6458d5b7ee11a281fc4ceec10987f1a9
siratNaqvi <sf591802@gmail.com>
2026-05-08 17:25:45 +0500

    fix: import entities directly for Vercel

--- backend/src/config/data-source.js
+++ backend/src/config/data-source.js
@@ -1,6 +1,13 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import dotenv from "dotenv";
+import { User } from "../entities/User.js";
+import { TrainingProgram } from "../entities/TrainingProgram.js";
+import { CourseModule } from "../entities/CourseModule.js";
+import { Enrollment } from "../entities/Enrollment.js";
+import { Quiz } from "../entities/Quiz.js";
+import { QuizResult } from "../entities/QuizResult.js";
+import { Certificate } from "../entities/Certificate.js";
 
 dotenv.config();
 
@@ -14,15 +21,7 @@ const AppDataSource = new DataSource({
   synchronize: true,
   logging: false,
   ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
-  entities: [
-    "src/entities/User.js",
-    "src/entities/TrainingProgram.js",
-    "src/entities/CourseModule.js",
-    "src/entities/Enrollment.js",
-    "src/entities/Quiz.js",
-    "src/entities/QuizResult.js",
-    "src/entities/Certificate.js"
-  ],
+  entities: [User, TrainingProgram, CourseModule, Enrollment, Quiz, QuizResult, Certificate],
 });
 
 export default AppDataSource;
